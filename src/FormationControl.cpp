05ef719b8fe3479b87c6d2bd463334c2c1a3f31d
Ivan Loncar <loncar.fer@gmail.com>
2015-10-21 16:35:59 +0200

    Added saturation. Stability problems.

--- src/FormationControl.cpp
+++ src/FormationControl.cpp
@@ -25,7 +25,6 @@ public:
 			FCGotState = new bool[VehNum];
 			VehState = new auv_msgs::NavSts[VehNum];
 			StateNode = new ros::Subscriber[VehNum];
-	//		NVeh = VehNum;
 			init();
 		}
 	}
@@ -67,6 +66,12 @@ public:
 		initialize_controller();
 		ROS_INFO("\n\n\n\nControler init finished...\n\n\n\n");
 
+//		for(int i=0; i <VehNum; i++) {
+//			for(int j=0; j<VehNum;j++){
+//				ROS_INFO("DGMat[%d][%d] = %d\tGMat[%d][%d] = %f\nFormX[%d][%d] = %f\tFormY[%d][%d] = %f\n", i, j, DGMat[i*VehNum + j], i, j,GMat[i*VehNum + j], i, j, FormX[i*VehNum + j], i, j, FormY[i*VehNum + j]);
+//			}
+//		}
+
 	}
 
 	void onEstimate(const auv_msgs::NavSts::ConstPtr& state, const int& i) {
@@ -96,30 +101,46 @@ public:
 
 	void ControlLaw() {
 
-		double XCurr, YCurr, Xi, Yi;
+		double XCurr, YCurr, Xi, Yi, G, FX, FY;
+		int DG;
 		XCurr = VehState[CurrentVeh].position.east;
 		YCurr = VehState[CurrentVeh].position.north;
 
-//		ROS_INFO("\nXCurr = %f \nYCurr = %f", XCurr, YCurr);
+//		ROS_INFO("XCurr = %f \tYCurr = %f", XCurr, YCurr);
 
 		for(int i=0; i<VehNum; i++){
 			if(i!=CurrentVeh){
 				Xi = VehState[i].position.east;
 				Yi = VehState[i].position.north;
-				ROS_INFO("Udaljenost od %d X = %f\n", i,XCurr - Xi + scaleForm('x',i));
-				ROS_INFO("Udaljenost od %d Y = %f\n", i,YCurr - Yi + scaleForm('y',i));
-				VelConReq.twist.linear.x = VelConReq.twist.linear.x - gain(i)*(XCurr - Xi + scaleForm('x',i));
-				VelConReq.twist.linear.y = VelConReq.twist.linear.y - gain(i)*(YCurr - Yi + scaleForm('y',i));
-//				ROS_INFO("\nVelX = %f \nVelY = %f", VelConReq.twist.linear.x, VelConReq.twist.linear.y);
+				DG = DGMat[CurrentVeh*VehNum + i];
+				G = GMat[CurrentVeh*VehNum + i];
+				FX = scaleX*FormX[CurrentVeh*VehNum + i];
+				FY = scaleY*FormY[CurrentVeh*VehNum + i];
+
+				ROS_INFO("Udaljenost od %d X = %f\n", i,XCurr - Xi);
+				ROS_INFO("Udaljenost od %d Y = %f\n", i,YCurr - Yi);
+				VelConReq.twist.linear.x = VelConReq.twist.linear.x - DG*G*(XCurr - Xi + FX);
+				VelConReq.twist.linear.y = VelConReq.twist.linear.y - DG*G*(YCurr - Yi + FY);
 			}
 		}
+
+		if(VelConReq.twist.linear.x > MaxSpeed[1])
+			VelConReq.twist.linear.x = MaxSpeed[1];
+		else if (VelConReq.twist.linear.x < -MaxSpeed[1])
+			VelConReq.twist.linear.x = -MaxSpeed[1];
+
+		if(VelConReq.twist.linear.y > MaxSpeed[2])
+			VelConReq.twist.linear.y = MaxSpeed[2];
+		else if (VelConReq.twist.linear.y < -MaxSpeed[2])
+			VelConReq.twist.linear.y = -MaxSpeed[2];
+
 		ROS_INFO("VelX = %f\n", VelConReq.twist.linear.x);
 		ROS_INFO("VelY = %f\n", VelConReq.twist.linear.y);
+
 		VelConNode.publish(VelConReq);
 //		ROS_INFO("\n\n\nKraj publishanja zeljene brzine\n\n\n");
 
 	}
-
 
 	double gain(const int& i) {
 		return DGMat[CurrentVeh*VehNum + i]*GMat[CurrentVeh*VehNum + i];
@@ -128,8 +149,10 @@ public:
 	double scaleForm(const char& a, const int& i) {
 		if(a=='x' || a=='X')
 			return scaleX*FormX[CurrentVeh*VehNum + i];
-		else
+		else {
 			return scaleY*FormY[CurrentVeh*VehNum + i];
+			ROS_INFO("\n\n\nRADIIII\n\n\n");
+		}
 	}
 
 	void onManRef(const std_msgs::Bool::ConstPtr& state) {}
@@ -141,9 +164,7 @@ public:
 
 	void reset(const auv_msgs::NavSts& ref, const auv_msgs::NavSts& state) {}
 
-
 	void formationChange() {}
-
 
 	void initialize_controller() {
 		ros::NodeHandle nh;
@@ -152,6 +173,9 @@ public:
 		nh.getParam("GMat", GMat);
 		nh.getParam("FormX", FormX);
 		nh.getParam("FormY", FormY);
+//		nh.param("nu_manual/maximum_speeds",MaxSpeed, {0.05,0.05,0.05,0,0,0.5});
+		nh.getParam("nu_manual/maximum_speeds",MaxSpeed);
+		ROS_INFO("MaxSped[1] = %f\tMaxSpeed[2] = %f",MaxSpeed[1], MaxSpeed[2]);
 		for(int i=0; i<VehNum;i++)
 			FCGotState[i] = false;
 		FCStart = false;
@@ -163,17 +187,18 @@ private:
 	ros::Publisher VelConNode;
 	auv_msgs::NavSts *VehState;
 	auv_msgs::BodyVelocityReq VelConReq;
+
 	bool FCEnable;
 	bool FCStart;
 	bool *FCGotState;
 	bool FCTempStart;
 	int VehNum;
 	int CurrentVeh;
-//	int NVeh;
 	std::vector<std::string> ParentNS;
 	std::vector<int> DGMat; // direct graph matrix
 	std::vector<double> GMat; // gain matrix
 	std::vector<double> FormX, FormY; // formation distances matrix
+	std::vector<double> MaxSpeed;
 };
 
 int main(int argc, char **argv)  {
