68a995c8ba3928ec88f5c6d8c4640d22d23620c4
Ivan Loncar <ivan.loncar@fer.hr>
2017-10-12 13:45:08 +0200

    reverted typo, fixed another typo, fixed logic in fetching reference data

--- src/FormationControl.cpp
+++ src/FormationControl.cpp
@@ -331,7 +331,7 @@ void FormControl::onState(const auv_msgs::NavSts::ConstPtr& state, const int& id
 	vehObj[inx].lastState = *state;
 	vehObj[inx].stateValid = true;
 
-	ROS_INFO("State id: %d inx: %d Pos: (%f, %f)",id, inx, state->position.north, state->position.east);
+	ROS_INFO("State id: %d Pos: (%f, %f)",id, state->position.north, state->position.east);
 
 	/* if state of the vehicle didn't came in last 2 seconds, disable controller for his measurements */
 	for(int i=0; i<vehObj.size(); i++) {
@@ -355,30 +355,31 @@ void FormControl::ControlLaw() {
 	double G, FX, FY, yawCurr, yaw, speedX, speedY;
 	double normalX, normalY, rij, frcX, frcY;
 	double rplFrcX(0.0), rplFrcY(0.0);
-	int DG;
+	int DG, vehNum;
 	ros::NodeHandle nh;
 	yCurr = vehObj[0].lastState.position.east;
 	xCurr = vehObj[0].lastState.position.north;
 	yawCurr = vehObj[0].lastState.orientation.yaw;
-
+	vehNum = (int) sqrt(formX.size());
 
 	velConReq.twist.linear.x = 0;
 	velConReq.twist.linear.y = 0;
 
+
 	for(int i=1; i<vehObj.size(); i++){
 		// if id is out of formation bounds print error
-		if(vehObj[i].id < (int) sqrt(formX.size()) ) {
+		if(vehObj[i].id < vehNum ) {
 			// if measurement is valid add contribution
 			if(vehObj[i].stateValid){
 				yi = vehObj[i].lastState.position.east;
 				xi = vehObj[i].lastState.position.north;
-				DG = DGMat[vehObj[i].id*vehObj.size() + vehObj[i].id];
-				G = GMat[vehObj[i].id*vehObj.size() + vehObj[i].id];
-				FX = scaleX*formX[vehObj[i].id*vehObj.size() + vehObj[i].id];
-				FY = scaleY*formY[vehObj[i].id*vehObj.size() + vehObj[i].id];
+				DG = DGMat[vehObj[0].id*vehNum + vehObj[i].id];
+				G = GMat[vehObj[0].id*vehNum + vehObj[i].id];
+				FX = scaleX*formX[vehObj[0].id*vehNum + vehObj[i].id];
+				FY = scaleY*formY[vehObj[0].id*vehNum + vehObj[i].id];
 
-				ROS_INFO("Pogreska po X %d = %f\n", i,xCurr - xi + FX);
-				ROS_INFO("Pogreska po Y %d = %f\n", i,yCurr - yi + FY);
+				//ROS_INFO("Pogreska po X %d = %f\n", i,xCurr - xi + FX);
+				//ROS_INFO("Pogreska po Y %d = %f\n", i,yCurr - yi + FY);
 
 				/* consensus control */
 				velConReq.twist.linear.x = velConReq.twist.linear.x - DG*G*(xCurr - xi + FX);
