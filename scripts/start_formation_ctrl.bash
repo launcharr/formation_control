4641b9b43b04232a9ce21470900646e411130202
Ivan Loncar <ivan.loncar@fer.hr>
2018-10-03 12:16:53 +0200

    Fixed bug with udp_relay by providing local address for packet self reject. Changed formation control information matrices. Modified script for starting formation control node.

--- scripts/start_formation_ctrl.bash
+++ scripts/start_formation_ctrl.bash
@@ -1,26 +1,26 @@
-source env_formation_veh_local.sh
+# load formation environment variables
+source env_formation_veh.sh
 
 BYOBU_SESSION_NAME=formation
+LOCAL_IP=10.0.10.41
+ROS2UDP_PORT=39876
 
 # get length of an array
 len=${#VEH_NAME[@]}
 
-#roslaunch -p $LOCAL_PORT formation_control formation_topside.launch
-byobu new-session -d -s "$BYOBU_SESSION_NAME" "roslaunch -p $LOCAL_PORT formation_control formation_topside.launch local_simulation:=false && basl -l"
+byobu new-session -d -s "$BYOBU_SESSION_NAME" "roslaunch -p $LOCAL_PORT formation_control formation_topside.launch local_simulation:=false local_address:=$LOCAL_IP local_port:=$ROS2UDP_PORT && bash -l"
 
 # start every formation control node
 for (( i=0; i<${len}; i++ ));
 do
-    comm="export ROS_MASTER_URI=http://${VEH_IP[$i]}:${VEH_PORT[$i]} && roslaunch formation_control formation_pladypos.launch veh_name:=${VEH_NAME[$i]} veh_id:=$i local_simulation:=false && bash -l"
-    #byobu new-window -n "${VEH_NAME[$i]} fc" -t "$BYOBU_SESSION_NAME" "$comm"
-    #TODO remotely start formation control 
+    comm="ssh -t stdops@${VEH_IP[$i]} 'source ~/subcultron_ws/devel/setup.bash && roslaunch formation_control formation_pladypos.launch veh_name:=${VEH_NAME[$i]} veh_id:=$i local_simulation:=false local_address:=${VEH_IP[$i]} local_port:=$ROS2UDP_PORT && bash -l'"
+    byobu new-window -n "${VEH_NAME[$i]} fc" -t "$BYOBU_SESSION_NAME" "$comm"
 done
 
 # export ros master
 for (( i=0; i<${len}; i++ ));
 do
-    #byobu new-window -n "${VEH_NAME[$i]} echo" -t "formation" "echo $i && bash -l"
-    comm="export ROS_MASTER_URI=http://${VEH_IP[$i]}:${VEH_PORT[$i]} && bash -l"
+    comm="source ~/subcultron_ws/devel/setup.bash && export ROS_MASTER_URI=http://${VEH_IP[$i]}:${VEH_PORT[$i]} && bash -l"
     byobu new-window -n "${VEH_NAME[$i]} ${VEH_PORT[$i]}" -t "formation" "$comm"
 done
 
